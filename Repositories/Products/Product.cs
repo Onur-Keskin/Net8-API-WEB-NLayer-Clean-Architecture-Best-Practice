15741190ebdf39633b361a1c729513d85fdeb396
Hüseyin Onur Keskin <h.onur.keskin@hotmail.com>
2024-11-21 16:47:19 +0300

    SaveChanges Interceptor add.

--- Repositories/Products/Product.cs
+++ Repositories/Products/Product.cs
@@ -2,7 +2,7 @@
 
 namespace App.Repositories.Products
 {
-    public class Product
+    public class Product : IAuditEntity
     {
         public int Id { get; set; }
         public string Name { get; set; } = default!;
@@ -10,6 +10,7 @@ namespace App.Repositories.Products
         public int Stock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; } = default!; //Bir product' ın mutlaka bir category' si olmalı
-
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
     }
 }
